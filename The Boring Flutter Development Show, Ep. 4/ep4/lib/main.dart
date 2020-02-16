52a7f8cc519eb2476d158cce37c62998f70c5428
Yudhishthir Singh <singh.yudi10@gmail.com>
2020-02-16 14:04:09 +0530

    BLOC done

--- The Boring Flutter Development Show, Ep. 4/ep4/lib/main.dart
+++ The Boring Flutter Development Show, Ep. 4/ep4/lib/main.dart
@@ -3,8 +3,6 @@ import 'src/Article.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:async';
 
-import 'package:http/http.dart' as http;
-
 void main() => runApp(MyApp());
 
 //Change in my newer branch and we are trying "merge"
@@ -34,27 +32,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<int> _ids = [
-    21182705,
-    21181664,
-    21157404,
-    21161233,
-    21167871,
-    21160238,
-    21184623,
-    21172686,
-    21172936,
-    21182628
-  ];
-
-  Future<Article> _getArticle(int id) async {
-    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
-    final storyRes = await http.get(storyUrl);
-    if (storyRes.statusCode == 200) {
-      return parseArticle(storyRes.body);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- The Boring Flutter Development Show, Ep. 4/ep4/lib/src/hn_bloc.dart
+++ The Boring Flutter Development Show, Ep. 4/ep4/lib/src/hn_bloc.dart
@@ -1,9 +1,47 @@
+import 'dart:collection';
+
 import 'package:ep3/src/Article.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:http/http.dart' as http;
 
 class HackerNewsBloc {
   // Stream<List<Article>> get articles => _articlesSubject.stream;
   // final _articlesSubject = BehaviorSubject<List<Article>>();
   Stream<List<Article>> get articles => _articlesSubject.stream;
-  final _articlesSubject = BehaviorSubject<List<Article>>();
+  final _articlesSubject = BehaviorSubject<UnmodifiableListView<Article>>();
+
+  var _articles = <Article>[];
+
+  HackerNewsBloc() {
+    _updateArticles().then((_) {
+      _articlesSubject.add(UnmodifiableListView(_articles));
+    });
+  }
+
+  List<int> _ids = [
+    21182705,
+    21181664,
+    21157404,
+    21161233,
+    21167871,
+    21160238,
+    21184623,
+    21172686,
+    21172936,
+    21182628
+  ];
+
+  Future<Null> _updateArticles() async {
+    final futureArticles = _ids.map((id) => _getArticle(id));
+    final articles = await Future.wait(futureArticles);
+    _articles = articles;
+  }
+
+  Future<Article> _getArticle(int id) async {
+    final storyUrl = 'https://hacker-news.firebaseio.com/v0/item/$id.json';
+    final storyRes = await http.get(storyUrl);
+    if (storyRes.statusCode == 200) {
+      return parseArticle(storyRes.body);
+    }
+  }
 }
